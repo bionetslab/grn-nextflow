e0859a10430b888fba99c63de43579218d7f16d0
Nicolai Meyerhoefer <yb85avem@idea-ramses.aibe.uni-erlangen.de>
2023-10-25 16:47:23 +0000

    fixed small bug in diffcoex/zscore network computation

--- modules/tools/diffcoex/resources/usr/bin/run_diffcoex.R
+++ modules/tools/diffcoex/resources/usr/bin/run_diffcoex.R
@@ -23,7 +23,7 @@ cond_name_1 <- sub('.*out_', "", opt$input.file.1)
 cond_name_2 <- sub('.*out_', "", opt$input.file.2)
 
 cond_name_1 <- str_replace(cond_name_1, ".tsv", "")
-cond_name_1 <- str_replace(cond_name_1, ".tsv", "")
+cond_name_2 <- str_replace(cond_name_2, ".tsv", "")
 
 data.1 <- read.table(file = opt$input.file.1, sep = "\t", header = TRUE)
 data.2 <- read.table(file = opt$input.file.2, sep = "\t", header = TRUE)
@@ -41,43 +41,50 @@ adj_p <- dcAdjust(raw_p, f = p.adjust, method = 'fdr')
 dcnet <- dcNetwork(scores, adj_p)
 edgedf <- as_data_frame(dcnet, what = 'edges')
 
-edgedf$abs_score <- abs(edgedf$score)
-edgedf$condition <- ifelse(edgedf$score >= 0, cond_name_1, cond_name_2)
-edgedf <- setorder(edgedf, -abs_score)
-edgedf <- edgedf[1:opt$top_n_edges,]
-
-edgedf <- edgedf[, !(names(edgedf) %in% c("abs_score", "color"))]
-colnames(edgedf) <- c("target", "regulator", "weight", "condition")
-edgedf$effect <- 0
-
-for(row in 1:nrow(edgedf)) {
-    target <- edgedf[row, 1]
-    regulator <- edgedf[row, 2]
-    condition <- edgedf[row, 4]
-
-    if (condition == cond_name_1) {
-        target_data <- as.numeric(data.1[data.1$Gene == target,2:ncol(data.1)]) 
-        regulator_data <- as.numeric(data.1[data.1$Gene == regulator,2:ncol(data.1)])        
-    } else {
-        target_data <- as.numeric(data.2[data.2$Gene == target,2:ncol(data.2)]) 
-        regulator_data <- as.numeric(data.2[data.2$Gene == regulator,2:ncol(data.2)])        
+if (nrow(edgedf) > 0) {
+    edgedf$abs_score <- abs(edgedf$score)
+    edgedf$condition <- ifelse(edgedf$score >= 0, cond_name_1, cond_name_2)
+    edgedf <- setorder(edgedf, -abs_score)
+    if (nrow(edgedf) > opt$top_n_edges) {
+        edgedf <- edgedf[1:opt$top_n_edges,]
     }
 
-    # outlier detection (|Z-score| > 2 -> outlier)
-    target_mean <- mean(target_data)
-    regulator_mean <- mean(regulator_data)
+    edgedf <- edgedf[, !(names(edgedf) %in% c("abs_score", "color"))]
+    colnames(edgedf) <- c("target", "regulator", "weight", "condition")
+    edgedf$effect <- 0
 
-    target_sd <- sd(target_data)
-    regulator_sd <- sd(regulator_data)
+    for(row in 1:nrow(edgedf)) {
+        target <- edgedf[row, 1]
+        regulator <- edgedf[row, 2]
+        condition <- edgedf[row, 4]
 
-    target_mask <- !(target_data > (target_mean + 2*target_sd) | target_data < (target_mean - 2*target_sd)) 
-    regulator_mask <- !(regulator_data > (regulator_mean + 2*regulator_sd) | regulator_data < (regulator_mean - 2*regulator_sd))
+        if (condition == cond_name_1) {
+            target_data <- as.numeric(data.1[data.1$Gene == target,2:ncol(data.1)]) 
+            regulator_data <- as.numeric(data.1[data.1$Gene == regulator,2:ncol(data.1)])        
+        } else {
+            target_data <- as.numeric(data.2[data.2$Gene == target,2:ncol(data.2)]) 
+            regulator_data <- as.numeric(data.2[data.2$Gene == regulator,2:ncol(data.2)])        
+        }
 
-    mask <- regulator_mask & target_mask 
+        # outlier detection (|Z-score| > 2 -> outlier)
+        target_mean <- mean(target_data)
+        regulator_mean <- mean(regulator_data)
 
-    model <- lm(formula = regulator_data ~ target_data)
-    edgedf[row,]$effect <- as.numeric(model$coefficients[2])
+        target_sd <- sd(target_data)
+        regulator_sd <- sd(regulator_data)
+
+        target_mask <- !(target_data > (target_mean + 2*target_sd) | target_data < (target_mean - 2*target_sd)) 
+        regulator_mask <- !(regulator_data > (regulator_mean + 2*regulator_sd) | regulator_data < (regulator_mean - 2*regulator_sd))
+
+        mask <- regulator_mask & target_mask 
+
+        model <- lm(formula = regulator_data ~ target_data)
+        edgedf[row,]$effect <- as.numeric(model$coefficients[2])
+    }
+
+
+    fwrite(edgedf, paste0(opt$output.file, "aggregated_filtered_network_diffcoex.txt"), sep="\t")
+} else {
+    edgedf <- data.frame(target = NULL, source = NULL, weight = NULL, condition = NULL, effect = NULL)
+    fwrite(edgedf, paste0(opt$output.file, "aggregated_filtered_network_zscores.txt"), sep="\t")
 }
-
-
-fwrite(edgedf, paste0(opt$output.file, "aggregated_filtered_network_diffcoex.txt"), sep="\t")
